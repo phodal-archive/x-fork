3f2329056888758090dece60f4939f734465a9de
Fengda HUANG <h@phodal.com>
2015-07-14 21:38:23 +0800

    add cordova app loader

--- cyan/www/js/controllers.js
+++ cyan/www/js/controllers.js
@@ -1,8 +1,37 @@
 var app = angular.module('starter.controllers', ['ionic', 'ngCordova'])
 
-	.controller('AppCtrl', function ($scope) {
+	.controller('AppCtrl',['$q', function ($scope, $q) {
 		$scope.person = {fname: 'Clark', lname: 'Kent'};
-	});
+		$scope.onRefresh = function () {
+			console.log("update ready");
+			var fs = new CordovaPromiseFS({
+				Promise: $q
+			});
+			// Initialize a CordovaAppLoader
+			var loader = new CordovaAppLoader({
+				fs: fs,
+				serverRoot: 'http://app.phodal.com/',
+				localRoot: 'app',
+				cacheBuster: true, // make sure we're not downloading cached files.
+				checkTimeout: 10000 // timeout for the "check" function - when you loose internet connection
+			});
+			loader.check().then(function (updateAvailable) {
+				console.log(updateAvailable);
+				if (updateAvailable) {
+					loader.download(onprogress)
+						.then(
+						function (manifest) {
+							console.log(manifest);
+							loader.update();
+						},
+						function (failedDownloadUrlArray) {
+							console.log(failedDownloadUrlArray);
+						}
+					)
+				}
+			});
+		};
+	}]);
 
 var Hello = React.createClass({
 	propTypes: {
